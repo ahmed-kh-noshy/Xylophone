5d97276e83ed9a4efd2fb30e9ee388f5df2e088f
ahmed khaled <ahmedkhaled5562.ak@gmail.com>
2023-08-01 17:55:22 +0300

    adding project files and design screen

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by ahmed khaled on 01/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
